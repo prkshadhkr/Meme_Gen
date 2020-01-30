31a5cac27ce46820a0968b062fe881ab6c05cc29
Prakash Adhikari <prakash.ad@gmail.com>
2020-01-29 21:45:38 -0500

    added js in edit_js branch

---
+++ app.js
@@ -0,0 +1,81 @@
+const form = document.querySelector('#meme-form');
+const inputImg = document.querySelector('input[name="img-input"]');
+const inputTopText = document.querySelector('input[name="top-text"]');
+const inputBottomText = document.querySelector('input[name="bottom-text"]');
+const inputRange = document.querySelector('input[type="range"]');
+const inputColor = document.querySelector('input[type="color"]');
+const out = document.querySelector('.output');
+const imgCommonTexts = document.querySelectorAll('.img-text');
+const imgTopText = document.querySelector('.text-top');
+const imgBottomText = document.querySelector('.text-bottom');
+
+inputRange.oninput = function() {
+    out.textContent = inputRange.value;
+    for (let imgText of imgCommonTexts) {
+        imgText.style.fontSize = inputRange.value + "px";
+        imgText.style.color = inputColor.value;
+    }
+}
+
+form.addEventListener('submit', function(e) {
+    // form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const imgUpload = document.querySelector('#img-upload');
+    imgUpload.src = inputImg.value;
+    imgTopText.children[0].innerText = inputTopText.value;
+    imgBottomText.children[0].innerText = inputBottomText.value;
+});
+
+const imgSaveButton = document.querySelector('#img-save-btn');
+const box = document.querySelector('.box');
+const result = document.querySelector('.result');
+const removeButtons = document.querySelectorAll('.remove');
+
+
+imgSaveButton.addEventListener('click', function(e) {
+    e.preventDefault;
+    const imgSave = document.querySelector('#img-upload');
+    const imgTextStyleSave = document.querySelector('.img-text');
+    const imgTextTopSave = document.querySelector('.text-top').children[0];
+    const imgTextBottomSave = document.querySelector('.text-bottom').children[0];
+
+    const divBox = document.createElement('div');
+    const newImg = document.createElement('img');
+    const divTextTop = document.createElement('div');
+    const pTextTop = document.createElement('p');
+    const divTextBottom = document.createElement('div');
+    const pTextBottom = document.createElement('p');
+
+    const removeBtn = document.createElement('button');
+    removeBtn.innerText = "X";
+    removeBtn.classList.add('remove');
+
+    removeBtn.addEventListener('click', function(e) {
+        e.target.parentElement.parentElement.remove();
+    });
+
+    divBox.classList.add('box');
+
+    newImg.src = imgSave.src;
+    newImg.width = imgSave.width;
+    newImg.height = imgSave.height;
+
+    divTextTop.classList.add('text-top');
+    pTextTop.classList.add('img-text');
+    pTextTop.innerText = imgTextTopSave.innerText;
+    pTextTop.style.fontSize = imgTextStyleSave.style.fontSize;
+    pTextTop.style.color = imgTextStyleSave.style.color;
+
+    divTextBottom.classList.add('text-bottom');
+    pTextBottom.classList.add('img-text');
+    pTextBottom.innerText = imgTextBottomSave.innerText;
+    pTextBottom.style.fontSize = imgTextStyleSave.style.fontSize;
+    pTextBottom.style.color = imgTextStyleSave.style.color;
+
+    divTextTop.append(pTextTop);
+    divTextBottom.append(pTextBottom);
+    divTextBottom.append(removeBtn);
+    divBox.append(newImg, divTextTop, divTextBottom);
+    result.appendChild(divBox);
+
+});
